aa7c73fdbdf0d14e73eb03445544ea687599e968
sejuti <1605527@kiit.ac.in>
2019-01-23 08:59:25 -0800

    animations to be made

---
+++ career/js/blog.js
@@ -0,0 +1,59 @@
+
+$(document).ready(function()
+{
+	"use strict";
+
+	var header = $('.header');
+	var hamburgerBar = $('.hamburger_bar');
+	var hamburger = $('.hamburger');
+
+	setHeader();
+
+	$(window).on('resize', function()
+	{
+		setHeader();
+
+		setTimeout(function()
+		{
+			$(window).trigger('resize.px.parallax');
+		}, 375);
+	});
+
+	$(document).on('scroll', function()
+	{
+		setHeader();
+	});
+
+	initMenu();
+
+
+
+	function setHeader()
+	{
+		if($(window).scrollTop() > 91)
+		{
+			header.addClass('scrolled');
+			hamburgerBar.addClass('scrolled');
+		}
+		else
+		{
+			header.removeClass('scrolled');
+			hamburgerBar.removeClass('scrolled');
+		}
+	}
+
+
+	function initMenu()
+	{
+		if($('.menu').length)
+		{
+			var menu = $('.menu');
+			hamburger.on('click', function()
+			{
+				hamburger.toggleClass('active');
+				menu.toggleClass('active');
+			});
+		}
+	}
+
+});
